8b9690baa6a09b8eb7bd6c9e9b69928dbb830311
sadique567 <mohdsadique008@gmail.com>
2026-02-21 15:49:01 +0530

    add Coin

--- src/controllers/mycoin.controller.js
+++ src/controllers/mycoin.controller.js
@@ -2,11 +2,8 @@
 
 import cryptoAuthService from "../services/myCoin.service.js";
 
-
 export const addCryptoCoin = async (req, res) => {
-
   try {
-
     const data = await cryptoAuthService.addCryptoCoin(req.body);
 
     res.json({
@@ -14,40 +11,27 @@ export const addCryptoCoin = async (req, res) => {
       message: "Coin Added Successfully",
       data,
     });
-
   } catch (err) {
-
     res.status(400).json({
       status: false,
       message: err.message,
     });
-
   }
-
 };
 
-
-
-
 export const getAllCoin = async (req, res) => {
-
   try {
-
     const data = await cryptoAuthService.getAllCoin();
 
     res.json({
-      status: true,
+      status: "success",
       message: "Coin List Fetch Successfully",
       data,
     });
-
   } catch (err) {
-
     res.status(500).json({
       status: false,
       message: err.message,
     });
-
   }
-
 };
