bd0c9bdb8f917913a870c074c4fde1c89769dcf0
sadique567 <mohdsadique008@gmail.com>
2026-02-23 16:33:44 +0530

    added mucoin field in cryptowithaddress

--- src/models/cryptoWithAddress.model.js
+++ src/models/cryptoWithAddress.model.js
@@ -1,6 +1,54 @@
 import mongoose from "mongoose";
 
 const cryptowithAddress = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+
+  chain: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  symbol: {
+    type: String,
+    required: true,
+    uppercase: true,
+    trim: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  contract: {
+    type: String,
+    default: null,
+  },
+
+  decimals: {
+    type: Number,
+    default: 8,
+  },
+
+  coingecko_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  enabled_by_default: {
+    type: Boolean,
+    default: false,
+  },
+  // ----above model from mycoin.model.js
+
   cryptoName: {
     type: String,
     required: true,
